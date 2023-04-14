2a221ca9a20071b5555fc3d412406b09c2eb81ab
Xiahui Liu <coffeeliuxiahui@gmail.com>
2023-04-14 11:55:40 +0200

    use react useReducer to set notifications

--- anecdotes-query/src/App.js
+++ anecdotes-query/src/App.js
@@ -1,59 +1,89 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
-import { useQuery, useMutation, useQueryClient } from 'react-query'
-import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
+import React, { useReducer } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { createAnecdote, getAnecdotes, updateAnecdote } from "./requests";
 
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_NOTIFICATION":
+      return { message: `Added new anecdote: ${action.payload}` };
+    case "VOTE_NOTIFICATION":
+      return { message: `An anecdote is voted on: ${action.payload}`};
+    default:
+      return state;
+  }
+};
 
 const App = () => {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
+  const [notification, notificationDispatch] = useReducer(notificationReducer, {
+    message: null,
+  });
 
-  const newAnecdoteMutation = useMutation(createAnecdote,{
-    onSuccess:(newAnecdote) => {
-      const anecdotes = queryClient.getQueryData('anecdotes')
-      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
-    },
-  })
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      queryClient.setQueryData("anecdotes", (old) => [...old, newAnecdote]);
+      setTimeout(() => {
+        notificationDispatch({
+          type: "NEW_NOTIFICATION",
+          payload: newAnecdote.content,
+        });
+      }, 2000);
+    }
+  });
 
   const updateAnecdoteMutation = useMutation(updateAnecdote, {
     onSuccess: (updatedAnecdote) => {
-      queryClient.setQueryData('anecdotes', old => old.map(a => a.id === updatedAnecdote.id ? updatedAnecdote : a))
+      queryClient.setQueryData("anecdotes", (old) =>
+        old.map((a) =>
+          a.id === updatedAnecdote.id ? updatedAnecdote : a
+        )
+      );
+      setTimeout(() => {
+        notificationDispatch({
+          type: "VOTE_NOTIFICATION",
+          payload: updatedAnecdote.content,
+        });
+      }, 2000);
     },
-  })
+  });
+
+  const { data: anecdotes, isLoading } = useQuery("anecdotes", getAnecdotes, {
+    refetchOnWindowFocus: false,
+  });
 
   const handleVote = (anecdote) => {
-    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
-  }
-
-  const result = useQuery('anecdotes', getAnecdotes, {
-    refetchOnWindowFocus:false
-  })
-
-  if ( result.isLoading ) {
-    return <div>loading data...</div>
-  }
-
-  const anecdotes = result.data
+    updateAnecdoteMutation.mutate({
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    });
+  };
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
-      <Notification />
+
+      {notification.message && (
+        <Notification message={notification.message} />
+      )}
       <AnecdoteForm onSubmit={newAnecdoteMutation.mutate} />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+
+      {isLoading ? (
+        <div>loading data...</div>
+      ) : (
+        anecdotes.map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
+        ))
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- anecdotes-query/src/components/Notification.js
+++ anecdotes-query/src/components/Notification.js
@@ -1,18 +1,17 @@
-const Notification = () => {
+
+const Notification = ({ message }) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
     marginBottom: 5
-  }
+  };
   
-  if (true) return null
-
   return (
     <div style={style}>
-      
+      <span>{message}</span>
     </div>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
